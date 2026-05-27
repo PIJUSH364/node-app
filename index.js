70a492f79a2e8819d63b54e28643c4e46597671a
Pijush Ray Mondal <praymondal@gmail.com>
2026-05-27 15:22:51 +0530

    Add morgan middleware for logging HTTP requests

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 // Endpoint 1: GET /
 app.get('/', (req, res) => {
